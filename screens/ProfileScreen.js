7ce0cee5107b0cecbbf3cc45ad55900442332e3b
Emre Demir <emre.demir20@ogr.sakarya.edu.tr>
2021-01-01 14:36:26 +0300

    Profil Ekranı Oluşturuldu

---
+++ screens/ProfileScreen.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+export default class ProfileScreen extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text> Profil Ekranı</Text>
+            </View>
+        )
+    }
+}
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        alignItems:"center",
+        justifyContent:"center"
+    }
+});
